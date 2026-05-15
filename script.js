0dfa8a8b556570df5812ef9dc52ea2783956f1ee
Jordangerouz <jordangerouz@gmail.com>
2026-05-14 21:12:48 -0500

    added carousel

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello world!");
+// carousel stuff --------------------------------
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
