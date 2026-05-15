7dc8b3d990202b3f5b1b4b298752e9c9a6e50649
Jordangerouz <jordangerouz@gmail.com>
2026-05-14 22:28:09 -0500

    added a scroll to top button

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 console.log("Hello world!");
 // carousel stuff --------------------------------
-const myCarouselElement = document.querySelector('#myCarousel')
+// const myCarouselElement = document.querySelector('#myCarousel')
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 2000,
+//   touch: false
+// });
+
+// scroll to top --------------------------------
+const scrollToTopButton = document.getElementById('scroll-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 400) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
